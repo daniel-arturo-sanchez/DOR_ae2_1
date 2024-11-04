8d9df0ef2bd1076bde23a151bc83ebcc789f2628
2dawa <2dawa@informatica.edu>
2024-11-04 09:50:33 +0000

    Modificación funcionalidad del botón de cambio de modo

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 let isDarkTheme = document.body.classList.contains("dark-theme");
 const themeSwitch = document.getElementById("theme");
 
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    themeSwitch.checked = true;
+    document.body.setAttribute("class", "dark-theme");
+    isDarkTheme = document.body.classList.contains("dark-theme");
+} else if (window.matchMedia('(prefers-color-scheme: light)').matches){
+    document.body.setAttribute("class", "light-theme");
+    isDarkTheme = document.body.classList.contains("dark-theme");
+    themeSwitch.checked = false;
+}
+
 function switchTheme() {
     if (isDarkTheme){
-        document.body.removeAttribute("class","dark-theme");
+        document.body.setAttribute("class", "light-theme");
         isDarkTheme = document.body.classList.contains("dark-theme");
     } else {
         document.body.setAttribute("class", "dark-theme");
