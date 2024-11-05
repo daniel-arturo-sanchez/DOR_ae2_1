48b771244d42889731f5ae3339ef4d60a1bdbe8f
2dawa <2dawa@informatica.edu>
2024-11-05 10:09:18 +0000

    Alto contraste

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-let isDarkTheme = document.body.classList.contains("dark-theme");
-const themeSwitch = document.getElementById("theme");
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    themeSwitch.checked = true;
-    document.body.setAttribute("class", "dark-theme");
-    isDarkTheme = document.body.classList.contains("dark-theme");
-} else if (window.matchMedia('(prefers-color-scheme: light)').matches){
-    document.body.setAttribute("class", "light-theme");
-    isDarkTheme = document.body.classList.contains("dark-theme");
-    themeSwitch.checked = false;
-}
+const themeSwitch = document.getElementById("theme");
+const btnHighContrast = document.getElementById("high-contrast");
+
+let isDarkTheme = document.body.classList.contains("dark-theme");
+let isHighContrast = document.body.classList.contains("high-contrast");
 
 function switchTheme() {
     if (isDarkTheme){
@@ -20,3 +14,32 @@ function switchTheme() {
         isDarkTheme = document.body.classList.contains("dark-theme");
     }
 }
+
+function switchHighContrast() {
+    if (isHighContrast){
+        if(isDarkTheme){
+            document.body.setAttribute("class", "dark-theme");
+        } else {
+            document.body.setAttribute("class", "light-theme");
+        }
+        isHighContrast = document.body.classList.contains("high-contrast");
+    } else {
+        if(isDarkTheme){
+            document.body.setAttribute("class", "dark-theme high-contrast");
+        } else {
+            document.body.setAttribute("class", "light-theme high-contrast");
+        }
+        isHighContrast = document.body.classList.contains("high-contrast");
+    }
+}
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    themeSwitch.checked = true;
+    document.body.setAttribute("class", "dark-theme");
+    isDarkTheme = document.body.classList.contains("dark-theme");
+} else {
+    document.body.setAttribute("class", "light-theme");
+    isDarkTheme = document.body.classList.contains("dark-theme");
+    themeSwitch.checked = false;
+}
+
