bd0d0324be0d2a6a3df16133d500095f1269bb3f
2dawa <2dawa@informatica.edu>
2024-11-08 13:05:49 +0000

    Patch: JS

--- main.js
+++ main.js
@@ -2,52 +2,51 @@
 const themeSwitch = document.getElementById("theme");
 const btnHighContrast = document.getElementById("high-contrast");
 
-let isDarkTheme = document.body.classList.contains("dark-theme");
-let isHighContrast = document.body.classList.contains("dark-theme-high-contrast") || document.body.classList.contains("light-theme-high-contrast");
+const isDarkTheme = () => document.body.classList.contains("dark-theme") || document.body.classList.contains("dark-theme-high-contrast");
+const isHighContrast = () => document.body.classList.contains("dark-theme-high-contrast") || document.body.classList.contains("light-theme-high-contrast");
+
+isDarkTheme;
+isHighContrast;
 
 function switchTheme() {
-    if (isDarkTheme){
-        if(isHighContrast){
+    if (isDarkTheme()){
+        if(isHighContrast()){
             document.body.setAttribute("class", "light-theme-high-contrast");
         } else {
             document.body.setAttribute("class", "light-theme");
         }
     } else {
-        if(isHighContrast){
+        if(isHighContrast()){
             document.body.setAttribute("class", "dark-theme-high-contrast");
         } else {
             document.body.setAttribute("class", "dark-theme");
         }
     }
-    isHighContrast = document.body.classList.contains("dark-theme-high-contrast") || document.body.classList.contains("light-theme-high-contrast");
-    isDarkTheme = document.body.classList.contains("dark-theme");
 }
 
 function switchHighContrast() {
-    if (isHighContrast){
-        if(isDarkTheme){
+    if (isHighContrast()){
+        if(isDarkTheme()){
             document.body.setAttribute("class", "dark-theme");
         } else {
             document.body.setAttribute("class", "light-theme");
         }
     } else {
-        if(isDarkTheme){
+        if(isDarkTheme()){
             document.body.setAttribute("class", "dark-theme-high-contrast");
         } else {
             document.body.setAttribute("class", "light-theme-high-contrast");
         }
     }
-    isHighContrast = document.body.classList.contains("dark-theme-high-contrast") || document.body.classList.contains("light-theme-high-contrast");
-    isDarkTheme = document.body.classList.contains("dark-theme");
 }
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     themeSwitch.checked = true;
     document.body.setAttribute("class", "dark-theme");
-    isDarkTheme = document.body.classList.contains("dark-theme");
+    isDarkTheme;
 } else {
     document.body.setAttribute("class", "light-theme");
-    isDarkTheme = document.body.classList.contains("dark-theme");
+    isDarkTheme;
     themeSwitch.checked = false;
 }
 
