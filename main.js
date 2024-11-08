14345f10d45405314d5719d4feaf67873baedfd8
2dawa <2dawa@informatica.edu>
2024-11-08 12:08:16 +0000

    Fixed dark theme buttons

--- main.js
+++ main.js
@@ -3,16 +3,24 @@ const themeSwitch = document.getElementById("theme");
 const btnHighContrast = document.getElementById("high-contrast");
 
 let isDarkTheme = document.body.classList.contains("dark-theme");
-let isHighContrast = document.body.classList.contains("high-contrast");
+let isHighContrast = document.body.classList.contains("dark-theme-high-contrast") || document.body.classList.contains("light-theme-high-contrast");
 
 function switchTheme() {
     if (isDarkTheme){
-        document.body.setAttribute("class", "light-theme");
-        isDarkTheme = document.body.classList.contains("dark-theme");
+        if(isHighContrast){
+            document.body.setAttribute("class", "light-theme-high-contrast");
+        } else {
+            document.body.setAttribute("class", "light-theme");
+        }
     } else {
-        document.body.setAttribute("class", "dark-theme");
-        isDarkTheme = document.body.classList.contains("dark-theme");
+        if(isHighContrast){
+            document.body.setAttribute("class", "dark-theme-high-contrast");
+        } else {
+            document.body.setAttribute("class", "dark-theme");
+        }
     }
+    isHighContrast = document.body.classList.contains("dark-theme-high-contrast") || document.body.classList.contains("light-theme-high-contrast");
+    isDarkTheme = document.body.classList.contains("dark-theme");
 }
 
 function switchHighContrast() {
@@ -22,15 +30,15 @@ function switchHighContrast() {
         } else {
             document.body.setAttribute("class", "light-theme");
         }
-        isHighContrast = document.body.classList.contains("high-contrast");
     } else {
         if(isDarkTheme){
-            document.body.setAttribute("class", "dark-theme high-contrast");
+            document.body.setAttribute("class", "dark-theme-high-contrast");
         } else {
-            document.body.setAttribute("class", "light-theme high-contrast");
+            document.body.setAttribute("class", "light-theme-high-contrast");
         }
-        isHighContrast = document.body.classList.contains("high-contrast");
     }
+    isHighContrast = document.body.classList.contains("dark-theme-high-contrast") || document.body.classList.contains("light-theme-high-contrast");
+    isDarkTheme = document.body.classList.contains("dark-theme");
 }
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
